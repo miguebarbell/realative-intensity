f865a88917fadfc0f0addff0f664dc8ae7b5b06e
Miguel <miguel@debloat.us>
2022-04-20 16:27:27 -0400

    tuned formulas

--- src/helper/calculator.ts
+++ src/helper/calculator.ts
@@ -1,14 +1,19 @@
 // Contains all the function to calculate the intensity
+// for sets with repetitions less than 5, use eplay.
+// TODO add more formulas
 function epleyFormula(strain: number, reps: number): number {
-    if (reps <= 1) return strain;
+    if (reps === 0) return 0;
+    else if (reps === 1) return strain;
     return Math.round(strain * (1 + (reps/30)));
 }
 function brzyckiFormula(strain: number, reps: number): number {
-    return Math.round(strain * (36/37 - reps));
+    return Math.round(strain * (36/(37 - reps)));
 }
 
 export default function calculateRelativeIntensity(strain: number, reps: number): number {
-    if (reps > 3) return brzyckiFormula(strain, reps)
+    if (reps === 0) return 0;
+    else if (reps === 1) return strain;
+    else if (reps >= 5) return brzyckiFormula(strain, reps)
     return epleyFormula(strain, reps)
 }
 
